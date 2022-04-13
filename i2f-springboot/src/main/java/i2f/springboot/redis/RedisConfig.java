0527b35939ccb9a522b5a91b1a34202404812194
Ice2Faith <Ugex_Savelar@163.com>
2022-04-13 16:42:17 +0800

    redis config fix

--- i2f-springboot/src/main/java/i2f/springboot/redis/RedisConfig.java
+++ i2f-springboot/src/main/java/i2f/springboot/redis/RedisConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -23,6 +24,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Slf4j
 @Configuration
 @ConfigurationProperties("i2f.springboot.config.redis")
+@EnableRedisRepositories
 public class RedisConfig {
 
     @Autowired
