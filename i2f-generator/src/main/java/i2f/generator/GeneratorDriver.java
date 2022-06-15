e44d499f4b02d0cdb67a6ca839f841f2d4480cb1
Ice2Faith <Ugex_Savelar@163.com>
2022-06-15 20:12:42 +0800

    code generator support

--- i2f-generator/src/main/java/i2f/generator/GeneratorDriver.java
+++ i2f-generator/src/main/java/i2f/generator/GeneratorDriver.java
@@ -1,5 +1,6 @@
 package i2f.generator;
 
+import i2f.core.db.core.DbBeanResolver;
 import i2f.core.db.core.DbResolver;
 import i2f.core.db.data.TableMeta;
 import i2f.extension.template.velocity.VelocityGenerator;
@@ -18,8 +19,7 @@ import java.util.Map;
  * @desc
  */
 public class GeneratorDriver {
-    public static String generate(Connection conn, String tableName, String template, JavaCodeContext codeCtx) throws Exception {
-        TableMeta table = DbResolver.getTableMeta(conn, tableName);
+    public static String generate(TableMeta table, String template, JavaCodeContext codeCtx) throws Exception {
         TableContext tableCtx=TableContext.parse(table);
         Map<String,Object> map=new HashMap<>();
         map.put("code",codeCtx);
@@ -27,12 +27,27 @@ public class GeneratorDriver {
         String result = VelocityGenerator.render(template, map);
         return result;
     }
-    public static void batch(Connection conn,String tableName,String templatePath,String outputPath,JavaCodeContext codeCtx) throws SQLException, IOException {
-        TableMeta table = DbResolver.getTableMeta(conn, tableName);
+    public static void batch(TableMeta table,String templatePath,String outputPath,JavaCodeContext codeCtx) throws SQLException, IOException {
         TableContext tableCtx=TableContext.parse(table);
         Map<String,Object> map=new HashMap<>();
         map.put("code",codeCtx);
         map.put("table",tableCtx);
         VelocityGenerator.batchRender(templatePath,map, outputPath,"UTF-8");
+    }
+    public static String generate(Connection conn, String tableName, String template, JavaCodeContext codeCtx) throws Exception {
+        TableMeta table = DbResolver.getTableMeta(conn, tableName);
+        return generate(table,template,codeCtx);
+    }
+    public static void batch(Connection conn,String tableName,String templatePath,String outputPath,JavaCodeContext codeCtx) throws SQLException, IOException {
+        TableMeta table = DbResolver.getTableMeta(conn, tableName);
+        batch(table,templatePath,outputPath,codeCtx);
+    }
+    public static String generate(Class beanClass, String template, JavaCodeContext codeCtx) throws Exception {
+        TableMeta table = DbBeanResolver.getTableMeta(beanClass);
+        return generate(table,template,codeCtx);
+    }
+    public static void batch(Class beanClass,String templatePath,String outputPath,JavaCodeContext codeCtx) throws SQLException, IOException {
+        TableMeta table = DbBeanResolver.getTableMeta(beanClass);
+        batch(table,templatePath,outputPath,codeCtx);
     }
 }
