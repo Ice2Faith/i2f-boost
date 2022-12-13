d5e7f13e2ed02170491c7add014d0b1f15eec4ac
Ice2Faith <Ugex_Savelar@163.com>
2022-12-13 21:16:41 +0800

    concurrent sync collection adapt

---
+++ i2f-core/src/main/java/i2f/core/collection/SyncIterator.java
@@ -0,0 +1,44 @@
+package i2f.core.collection;
+
+import java.util.Iterator;
+import java.util.concurrent.locks.ReadWriteLock;
+
+public class SyncIterator<E> implements Iterator<E> {
+    protected volatile Iterator<E> iterator;
+    protected volatile ReadWriteLock lock;
+
+    public SyncIterator(Iterator<E> iterator, ReadWriteLock lock) {
+        this.iterator = iterator;
+        this.lock = lock;
+    }
+
+    @Override
+    public boolean hasNext() {
+        lock.readLock().lock();
+        try {
+            return this.iterator.hasNext();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public E next() {
+        lock.readLock().lock();
+        try {
+            return this.iterator.next();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public void remove() {
+        lock.writeLock().lock();
+        try {
+            this.iterator.remove();
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+}
---
+++ i2f-core/src/main/java/i2f/core/collection/SyncList.java
@@ -0,0 +1,247 @@
+package i2f.core.collection;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class SyncList<E> implements List<E> {
+    private volatile List<E> list;
+    private volatile ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    public SyncList(List<E> list) {
+        this.list = list;
+    }
+
+    @Override
+    public int size() {
+        lock.readLock().lock();
+        try {
+            return this.list.size();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        lock.readLock().lock();
+        try {
+            return this.list.isEmpty();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        lock.readLock().lock();
+        try {
+            return this.list.contains(o);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        lock.readLock().lock();
+        try {
+            return new SyncIterator<>(this.list.iterator(), lock);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public Object[] toArray() {
+        lock.readLock().lock();
+        try {
+            return this.list.toArray();
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        lock.readLock().lock();
+        try {
+            return this.list.toArray(a);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public boolean add(E e) {
+        lock.writeLock().lock();
+        try {
+            return this.list.add(e);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        lock.writeLock().lock();
+        try {
+            return this.list.remove(o);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        lock.readLock().lock();
+        try {
+            return this.list.containsAll(c);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        lock.writeLock().lock();
+        try {
+            return this.list.addAll(c);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        lock.writeLock().lock();
+        try {
+            return this.list.addAll(index, c);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        lock.writeLock().lock();
+        try {
+            return this.list.removeAll(c);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        lock.writeLock().lock();
+        try {
+            return this.list.retainAll(c);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public void clear() {
+        lock.writeLock().lock();
+        try {
+            this.list.clear();
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public E get(int index) {
+        lock.readLock().lock();
+        try {
+            return this.list.get(index);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public E set(int index, E element) {
+        lock.writeLock().lock();
+        try {
+            return this.list.set(index, element);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public void add(int index, E element) {
+        lock.writeLock().lock();
+        try {
+            this.list.add(index, element);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public E remove(int index) {
+        lock.writeLock().lock();
+        try {
+            return this.list.remove(index);
+        } finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        lock.readLock().lock();
+        try {
+            return this.list.indexOf(o);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        lock.readLock().lock();
+        try {
+            return this.list.lastIndexOf(o);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        lock.readLock().lock();
+        try {
+            return new SyncListIterator<>(this.list.listIterator(), lock);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        lock.readLock().lock();
+        try {
+            return new SyncListIterator<>(this.list.listIterator(index), lock);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        lock.readLock().lock();
+        try {
+            return this.list.subList(fromIndex, toIndex);
+        } finally {
+            lock.readLock().unlock();
+        }
+    }
+}
