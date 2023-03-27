397b09ddb7ea398c041a19d26931c9e67fcb2756
Ice2Faith <Ugex_Savelar@163.com>
2023-03-27 11:32:45 +0800

    mysql backup suit

--- i2f-base/doc/env/database/rdb/mysql/mysql8-backup-suit/backups.bat
+++ i2f-base/doc/env/database/rdb/mysql/mysql8-backup-suit/backups.bat
@@ -7,13 +7,17 @@ set bak_dir=.\db_backup
 :: 获取完整路径
 for %%a in (%bak_dir%) do set bak_dir=%%~fa
 
+:: 表列表文件
+set _tables_list_file=%cd%\tables.txt
 
 set _backups_ret_path=%cd%
 cd .\mysql8\bin
 
-:: add your table blow here like this
-call bak.bat biz_car_info %bak_dir%
-
-
+:: load from tables.txt
+:: add your table names into tables.txt
+:: every line is an table
+for /f "delims=" %%i in (%_tables_list_file%) do (
+    call bak.bat %%i %bak_dir%
+)
 
 cd %_backups_ret_path%
--- i2f-base/doc/env/database/rdb/mysql/mysql8-backup-suit/backups.sh
+++ i2f-base/doc/env/database/rdb/mysql/mysql8-backup-suit/backups.sh
@@ -7,17 +7,19 @@ bak_dir=./backup_db
 # 获取完整路径
 bak_dir=`realpath $bak_dir`
 
+# 表列表文件
+_tables_list_file=`pwd`/tables.txt
+
 _backups_ret_path=`pwd`
 cd ./mysql8/bin
 chmod a+x ./bak.sh
 
-# add your table blow here like this
-./bak.sh biz_car_info "${bak_dir}"
-
-
-
-
-
-
+# load from tables.txt
+# add your table names into tables.txt
+# every line is an table
+for line in `cat $_tables_list_file`
+do
+  ./bak.sh $line "${bak_dir}"
+done
 
 cd $_backups_ret_path
