bb648b457b3abf1028e3cac60eeaf3076620aecc
Ice2Faith <Ugex_Savelar@163.com>
2023-08-22 22:40:30 +0800

    login guarder update

--- i2f-springboot/src/main/java/i2f/springboot/security/def/guarder/LoginLockBeforeLoginChecker.java
+++ i2f-springboot/src/main/java/i2f/springboot/security/def/guarder/LoginLockBeforeLoginChecker.java
@@ -1,7 +1,7 @@
 package i2f.springboot.security.def.guarder;
 
-import i2f.core.exception.BoostException;
 import i2f.extension.guarder.LoginGuarder;
+import i2f.springboot.security.exception.BoostAuthenticationException;
 import i2f.springboot.security.impl.BeforeLoginChecker;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,14 +38,14 @@ public class LoginLockBeforeLoginChecker implements BeforeLoginChecker {
     public void onLogin(String username, HttpServletRequest request) {
         LoginGuarder.LockType type = loginGuarder.check(request, username);
         if (type == LoginGuarder.LockType.USERNAME) {
-            throw new BoostException("您的账号已被锁定，请在" +
+            throw new BoostAuthenticationException("您的账号已被锁定，请在" +
                     loginGuarder.getCache().getExpire(
                             loginGuarder.usernameCacheKey(username),
                             TimeUnit.SECONDS) +
                     "秒后重试");
         } else if (type == LoginGuarder.LockType.IP) {
             String ip = LoginGuarder.getIp(request);
-            throw new BoostException("您的IP已被锁定，请在" +
+            throw new BoostAuthenticationException("您的IP已被锁定，请在" +
                     loginGuarder.getCache().getExpire(
                             loginGuarder.ipCacheKey(ip),
                             TimeUnit.SECONDS) +
