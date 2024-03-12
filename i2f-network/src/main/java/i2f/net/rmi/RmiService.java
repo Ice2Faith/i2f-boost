b2b01aca592d5db6d3f79979961aec00da580286
Ice2Faith <Ugex_Savelar@163.com>
2024-03-12 10:44:42 +0800

    network udp/rmi

---
+++ i2f-network/src/main/java/i2f/net/rmi/RmiService.java
@@ -0,0 +1,12 @@
+package i2f.net.rmi;
+
+import java.io.Serializable;
+import java.rmi.Remote;
+
+/**
+ * @author Ice2Faith
+ * @date 2024/3/12 10:34
+ * @desc
+ */
+public interface RmiService extends Remote {
+}
