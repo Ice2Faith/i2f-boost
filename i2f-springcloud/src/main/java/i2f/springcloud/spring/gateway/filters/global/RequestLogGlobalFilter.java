71c2ce8c06812fc9db05fb0cba4a5efe6f9da29e
Ice2Faith <Ugex_Savelar@163.com>
2024-04-01 17:05:06 +0800

    springcloud

--- i2f-springcloud/src/main/java/i2f/springcloud/spring/gateway/filters/global/RequestLogGlobalFilter.java
+++ i2f-springcloud/src/main/java/i2f/springcloud/spring/gateway/filters/global/RequestLogGlobalFilter.java
@@ -32,56 +32,56 @@ import java.util.UUID;
 @ConfigurationProperties(prefix = "springcloud.config.gateway.global.filters.request-log")
 public class RequestLogGlobalFilter implements GlobalFilter, Ordered {
 
-    private boolean showHeaders=false;
+    private boolean showHeaders = false;
 
-    private boolean showQuerys=true;
+    private boolean showQuerys = false;
 
-    public static final String EXCHANGE_BEGIN_TIME_KEY="exchangeBeginTime";
+    public static final String EXCHANGE_BEGIN_TIME_KEY = "exchangeBeginTime";
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
         ServerHttpResponse response = exchange.getResponse();
+        ServerHttpRequest request = exchange.getRequest().mutate().header("trace-id", new String[]{UUID.randomUUID().toString()}).build();
 
-        exchange.getAttributes().put(EXCHANGE_BEGIN_TIME_KEY,System.currentTimeMillis());
 
-        request.getHeaders().add("trace-id", UUID.randomUUID().toString());
+        exchange.getAttributes().put(EXCHANGE_BEGIN_TIME_KEY, System.currentTimeMillis());
+
 
         RequestPath path = request.getPath();
-        StringBuilder builder=new StringBuilder();
+        StringBuilder builder = new StringBuilder();
         builder.append("\n-----------global filter begin------------").append("\n");
-        builder.append("request path:"+path).append("\n");
+        builder.append("request path:" + path).append("\n");
 
-        builder.append("request method:"+request.getMethod().name()).append("\n");
-        if(showHeaders){
+        builder.append("request method:" + request.getMethod().name()).append("\n");
+        if (showHeaders) {
             builder.append("request header:").append("\n");
             HttpHeaders headers = request.getHeaders();
-            for(String item : headers.keySet()){
-                List<String> vals=headers.get(item);
-                for(String val : vals){
-                    builder.append("\t"+item+":"+val).append("\n");
+            for (String item : headers.keySet()) {
+                List<String> vals = headers.get(item);
+                for (String val : vals) {
+                    builder.append("\t" + item + ":" + val).append("\n");
                 }
             }
         }
 
-        if(showQuerys){
+        if (showQuerys) {
             builder.append("request query:").append("\n");
             MultiValueMap<String, String> query = request.getQueryParams();
-            for(String item : query.keySet()){
-                List<String> vals=query.get(item);
-                for(String val : vals){
-                    builder.append("\t"+item+":"+val).append("\n");
+            for (String item : query.keySet()) {
+                List<String> vals = query.get(item);
+                for (String val : vals) {
+                    builder.append("\t" + item + ":" + val).append("\n");
                 }
             }
         }
 
-        log.info(builder.toString()+"-----------global filter pending ------------\n");
-        return chain.filter(exchange).then(Mono.fromRunnable(()->{
-            Long beginTime=exchange.getAttribute(EXCHANGE_BEGIN_TIME_KEY);
-            if(beginTime==null){
+        log.info(builder.toString() + "-----------global filter pending ------------\n");
+        return chain.filter(exchange.mutate().request(request).build()).then(Mono.fromRunnable(() -> {
+            Long beginTime = exchange.getAttribute(EXCHANGE_BEGIN_TIME_KEY);
+            if (beginTime == null) {
                 return;
             }
-            builder.append("process time:\t"+(System.currentTimeMillis()-beginTime)+"ms").append("\n");
+            builder.append("process time:\t" + (System.currentTimeMillis() - beginTime) + "ms").append("\n");
             builder.append("-----------global filter end------------").append("\n");
 
             log.info(builder.toString());
@@ -93,3 +93,4 @@ public class RequestLogGlobalFilter implements GlobalFilter, Ordered {
         return -1;
     }
 }
+
